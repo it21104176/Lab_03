084444edac3fc0ca2e85c8f4ece0a52dee1d9e70
Binuja <it21104176@my.sliit.lk>
2022-08-15 11:11:10 +0530

    Main interface

---
+++ app/src/main/java/com/example/myapplication/LabSheet03.java
@@ -0,0 +1,14 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LabSheet03 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lab_sheet03);
+    }
+}
